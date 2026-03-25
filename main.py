946411d898e3bad103cafaeb3021bc23a33b4b4a
vitor.chagas <vitor.chagas@altave.com.br>
2026-03-24 22:51:36 -0300

    feat: Add pre-commit files and makefile

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import cv2
+
 from src.exercises.squat_detector import SquatDetector
 
 
-def main():
-    
-    cap = cv2.VideoCapture("videos/input/video-1.mp4") 
+def main() -> None:
+    """Run the squat detector on a video file.
+
+    Returns
+    -------
+        None
+
+    """
+    cap = cv2.VideoCapture("videos/input/video-1.mp4")
 
     detector = SquatDetector()
 
@@ -16,11 +23,9 @@ def main():
         list_points = detector.find_points(frame)
         print(list_points)
 
-        cv2.imshow('Squat Detector', frame) 
+        cv2.imshow("Squat Detector", frame)
 
         cv2.waitKey(1)
-
-
 
 
 if __name__ == "__main__":
--- src/exercises/squat_detector.py
+++ src/exercises/squat_detector.py
@@ -1,56 +1,97 @@
 import cv2
-import mediapipe as mp 
+import mediapipe as mp
 
 
 class SquatDetector:
-    def __init__(self, detector_conf=0.5,
-                 traking_conf=0.5, color_points=(0,0,255,), color_conections=(255,255,255)
-                 ):
+    """Class to detect squats in a video."""
 
-        self.detector_conf=detector_conf
-        self.tracking_conf=traking_conf
-        self.color_points=color_points
-        self.color_conections=color_conections
-        
-        
+    def __init__(
+        self,
+        detector_conf: float = 0.5,
+        traking_conf: float = 0.5,
+        color_points: tuple = (0, 0, 255),
+        color_conections: tuple = (255, 255, 255),
+    ):
+        """Initialize the squat detector.
+
+        Args:
+        ----
+            detector_conf (float, optional): Min confidence for the pose detection. Defaults to 0.5
+            traking_conf (float, optional): Min confidence for the pose tracking. Defaults to 0.5
+            color_points (tuple, optional): Color for the points. Defaults to (0, 0, 0)
+            color_conections (tuple, optional): Color for the connections. Defaults to (255, 255, 255)
+
+        Returns:
+        -------
+            None
+
+        """
+        self.detector_conf = detector_conf
+        self.tracking_conf = traking_conf
+        self.color_points = color_points
+
+        self.color_conections = color_conections
 
         self.mp_pose = mp.solutions.pose
-        self.pose = self.mp_pose.Pose(min_detection_confidence=self.detector_conf,
-                                    min_tracking_confidence=self.tracking_conf, model_complexity=1,
-                                    smooth_landmarks=True)     
+        self.pose = self.mp_pose.Pose(
+            min_detection_confidence=self.detector_conf,
+            min_tracking_confidence=self.tracking_conf,
+            model_complexity=1,
+            smooth_landmarks=True,
+        )
 
-
-        self.mp_drawing = mp.solutions.drawing_utils  
+        self.mp_drawing = mp.solutions.drawing_utils
 
         self.mp_drawing_config_points = self.mp_drawing.DrawingSpec(color=self.color_points)
 
-        self.mp_drawing_config_conections = self.mp_drawing.DrawingSpec(color=self.color_conections, )
+        self.mp_drawing_config_conections = self.mp_drawing.DrawingSpec(
+            color=self.color_conections,
+        )
 
+    def detect(self, frame: cv2.Mat, drawing: bool = True) -> cv2.Mat:
+        """Detect the pose in the given frame.
 
+        Args:
+        ----
+            frame (cv2.Mat): The frame to detect the pose in
+            drawing (bool, optional): Whether to draw the pose landmarks and connections
 
-    def detect(self, frame, drawing=True):          
+        Returns:
+        -------
+            cv2.Mat: The frame with the detected pose.
+
+        """
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
         self.result = self.pose.process(frame_rgb)
-        print(self.result)
 
         if self.result.pose_landmarks.landmark:
             if drawing:
-                self.mp_drawing.draw_landmarks(frame, 
-                                                self.result.pose_landmarks,
-                                                self.mp_pose.POSE_CONNECTIONS,
-                                                self.mp_drawing_config_points, 
-                                                self.mp_drawing_config_conections) 
+                self.mp_drawing.draw_landmarks(
+                    frame,
+                    self.result.pose_landmarks,
+                    self.mp_pose.POSE_CONNECTIONS,
+                    self.mp_drawing_config_points,
+                    self.mp_drawing_config_conections,
+                )
 
         return frame
-    
 
-    def find_points(self, frame, drawing=True):
+    def find_points(self, frame: cv2.Mat) -> list:
+        """Find the pose landmarks in the given frame.
+
+        Args:
+        ----
+            frame (cv2.Mat): The frame to find the pose landmarks in
+
+        Returns:
+        -------
+            list: A list of the pose landmarks in the format [id, x, y]
+
+        """
         list_points = []
         if self.result.pose_landmarks.landmark:
-
-           for id, points in enumerate(self.result.pose_landmarks.landmark):
-                h ,w,c = frame.shape
+            for id, points in enumerate(self.result.pose_landmarks.landmark):
+                h, w, _ = frame.shape
                 cx, cy = int(points.x * w), int(points.y * h)
 
                 list_points.append([id, cx, cy])
